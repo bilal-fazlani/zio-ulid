e88c440c513b6817eacb8775f8f38a892055ff51
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-01-17 15:14:42 +0530

    upgrade libs

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 object Libs {
-  private lazy val zioVersion = "2.0.5"
+  private lazy val zioVersion = "2.0.6"
 
   lazy val zio = "dev.zio" %% "zio" % zioVersion
-  lazy val zioDirect = "dev.zio" %% "zio-direct" % "1.0.0-RC3"
+  lazy val zioDirect = "dev.zio" %% "zio-direct" % "1.0.0-RC4"
   lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
   lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt" % zioVersion
 }
